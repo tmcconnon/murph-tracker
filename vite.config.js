f7b098a48588146171181e9471bf232fb7dca87d
Tom McConnon <tmcconnon@gmail.com>
2025-06-06 20:26:33 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  root: '.',
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
+    rollupOptions: {
+      input: './index.html'
+    }
   }
 })
